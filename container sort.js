c70f2b809cc7a3644c7f7d35250825b96a38ff25
YyJqW <57286294+YyJqW@users.noreply.github.com>
2020-01-11 10:22:41 +0800

    1

--- container sort.js
+++ container sort.js
@@ -2,7 +2,7 @@ var rOOms = ['E21S38','E22S38','E23S39','E22S36'];
 var miner_container = ['5dcbaa0597e15e82348f25ec','5dcbbcd91a565749f3e49629','5dce2e891a41fb6f6e70d823','5de0d75b9276c577635ded73','5e00a8c3fb940e97e46bfd08','5e00d0ebf9d86bd2b651a00f'];
 var miner_container_o = ['5e0d9f0446f9f55ce46a437a',
 '5e0da23fd2e4535720b38c7f','5e0dd2d0864eb4b36c18214d','5e0ecd6d75a77b686d4e8e68','5e0ee3791b057b3f56e73b82',
-'5e0da7ccf9d0a64fb8667ab1','5e0da5e479557c41c83d218a','5e0db0b4f9f7f308c08201cb','5e0daee2d2e4539c47b391b8'];
+'5e0da7ccf9d0a64fb8667ab1','5e0da5e479557c41c83d218a','5e0db0b4f9f7f308c08201cb','5e0daee2d2e4539c47b391b8','5e0f4020d705e4dd2391696a'];
 var storage_container = ['5dcd01d5fbe41e2919f2de3c','5dd14b4d8b1d9b586a08b4e6','5de2c45198f152653b03e0c9','5e000e2bca55d7509867bac7','5e0f52ba29303d2a68445395'];
 var mineral_container = ['5dde4a3d26efdb785b8cef38','5dde676abca74326c19ee604','5df1d6e37cdbdb534abff006'];
 var miner_container_unlinked = ['5e00a8c3fb940e97e46bfd08'];
@@ -28,6 +28,7 @@ var CS=
         {
             for (var name in miner_container_o)
             {
+                if (Game.getObjectById(miner_container_o[name])!=null)
                 array[name]=Game.getObjectById(miner_container_o[name]);
             }
         }
