44ff44b6f238765af3af29ea796e3a27be688839
YyJqW <57286294+YyJqW@users.noreply.github.com>
2020-01-16 15:35:16 +0800

    1

--- creep auto generation.js
+++ creep auto generation.js
@@ -88,7 +88,7 @@ var CAG = {
             spawn.spawnCreep([CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,
             MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE], newName, {
                 memory: {
-                    role: 'transport',home:spawn
+                    role: 'transport',home:spawn,lab:false,trans:true,done:true
                 }
             });//900
          //自动生成搬运工人
