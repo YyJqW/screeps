104c152e323f1037a39a5a1996c1aaa07d5ecd67
YyJqW <57286294+YyJqW@users.noreply.github.com>
2020-01-19 17:58:21 +0800

    Update creep auto generation.js

--- creep auto generation.js
+++ creep auto generation.js
@@ -48,8 +48,8 @@ var CAG = {
             }
         }
         }
-        console.log('H=',harvesters.length,' U=',upers.length,' T=',transporter.length,' B=',builders.length,' T_i=',transporter_i.length,
-        ' M=',miner.length,' Claim=',claimer.length,' oH=',harvesters_o.length,' OT=',transporter_o.length,'Guardian=',guardian.length,'TM= ',transporter_m.length);
+        /*console.log('H=',harvesters.length,' U=',upers.length,' T=',transporter.length,' B=',builders.length,' T_i=',transporter_i.length,
+        ' M=',miner.length,' Claim=',claimer.length,' oH=',harvesters_o.length,' OT=',transporter_o.length,'Guardian=',guardian.length,'TM= ',transporter_m.length);*/
         if (guardian.length<guardiannum) {
             var newName = 'guardian' + Game.time;
             console.log('Spawning new attacker: ' + newName);
