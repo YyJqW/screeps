2b9b849c78ed8b6113301cb34cd4efb37c427f91
YyJqW <57286294+YyJqW@users.noreply.github.com>
2020-01-23 23:34:20 +0800

    undo sign

--- role.Claimer.js
+++ role.Claimer.js
@@ -19,7 +19,6 @@ var roleClaim =
             if(Game.rooms[rOom[creep.memory.number]]!=undefined&&creep.reserveController(Game.rooms[rOom[creep.memory.number]].controller)==ERR_NOT_IN_RANGE)
             {
            creep.moveTo(Game.rooms[rOom[creep.memory.number]].controller, {visualizePathStyle: {stroke: '#F0F8FF'},reusePath: 10});
-           creep.signController(Game.rooms[rOom[creep.memory.number]].controller,'Dont be rude,we will be good neighbor.Have auto hostile creep search.Avoiding this room if u are not hostile');
             }
     }
     };
